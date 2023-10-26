cb4b258303495237c2a2304b1d7e16c3daefe833
YUBEENLIM <bgs0782@gmail.com>
2023-10-26 09:32:33 +0900

    1단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,2 +1,19 @@
-package com.ll;public class App {
+package com.ll;
+
+import java.util.Scanner;
+
+public class App {
+    void run(){
+        System.out.println("== 명언 앱 ==");
+        while(true) {
+            System.out.print("명령) ");
+
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+            if(cmd.equals("종료")){
+                break;
+            }
+            System.out.println(cmd);
+        }
+    }
 }
