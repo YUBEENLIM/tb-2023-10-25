ed071e3e93d729c99210370de7520ab673c71a78
YUBEENLIM <bgs0782@gmail.com>
2023-10-26 18:02:33 +0900

    5단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,10 +1,14 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     void run(){
         int n = 0;
+
+        List<Quato> quatos = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
@@ -18,19 +22,32 @@ public class App {
                 break;
             }
 
-            if(cmd.equals("등록"))
+            else if(cmd.equals("등록")){
 
                 System.out.print("명언 : ");
-                String a = scanner.nextLine();
+                String content = scanner.nextLine();
                 System.out.print("작가 : ");
-                String b = scanner.nextLine();
+                String authorName = scanner.nextLine();
                 n++;
+                int id = n;
+                Quato quato = new Quato(id, content, authorName);
+                quatos.add(quato);
+
+
                 System.out.println(n + "번 명언이 등록되었습니다.");
 
-
-
-
+                }
+                else if(cmd.equals("목록")){
+                    for(int i = quatos.size() - 1; i>=0; i--){
+                        Quato quato = quatos.get(i);
+                        System.out.printf("%d/ %s / %s\n", quato.id, quato.content, quato.authorName);
+                    }
             }
         }
-    }
+
+            }
+}
+
+
+
 
---
+++ src/main/java/com/ll/Quato.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+public class Quato {
+    int id;
+    String content;
+    String authorName;
+
+    public Quato(int id, String content, String authorName){
+        this.id = id;
+        this.content = content;
+        this.authorName = authorName;
+    }
+}
